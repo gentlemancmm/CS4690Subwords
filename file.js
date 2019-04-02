dc64aa0424356c5cab9adbf9f7ffee41c15a1c76
Chris <comesthefuture@yahoo.com>
2019-04-01 22:40:51 -0600

    After closing time coding!

--- file.js
+++ file.js
@@ -5,5 +5,23 @@ const Path = require ('path')
 const Axios = require('axios')
 
 async function downloadImage(){
-    const url = 
+    const url = 'https://unsplash.com/photos/AaEQmoufHLk/download?force=true'
+    //This is a test image for the code.
+    const path = Path.resolve(__dirname, 'images', 'code.jpg')
+    const writer = Fs.createWriteStream(path)
+
+    const response = await Axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    })
+
+    response.data.pipe(writer)
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('error', reject)
+    })
 }
+
+await downloadImage()
